05754b389d4805994a2a7eb4e3e5720e08bd0fed
duchuykg <100073936+duchuykg@users.noreply.github.com>
2022-11-11 18:55:01 +0700

    Add files via upload

---
+++ views/main/about/index.php
@@ -0,0 +1,79 @@
+<?php
+  include_once('views/main/navbar.php');
+?>
+   <section id="team-member">
+      <div class="team text-center pb-5 pt-5">
+         <div class="container pb-5 pt-5">
+            <h2 class="fw-bold">Meet The Team</h2>
+            <p class="text-black-100 mb-5">Xin chao cac ban</p>
+            <div class="row">
+   
+               <div class="card-block col-md-8 col-lg-3 d-flex align-items-stretch">
+                  <div class="card">
+                     <div class="card-header bg-white card-header-custom">Leader</div>
+                     <img src="assets/images/qc.png" alt="" class="card-img-top">
+                     <div class="card-body bg-white" >
+                        <h5 class="card-title">Nguyễn Mậu Minh Đức</h5>
+                        <p class="card-text">Xin chào mọi người, mình là Minh Đức đây!
+                        </p>
+                     </div>
+                     <div class="card-footer bg-white">
+                        <i class="bi-facebook"></i>
+                        <a href="#" class="btn btn-primary">More Infor</a>
+                     </div>
+                  </div>
+               </div>
+   
+               <div class="card-block col-md-8 col-lg-3 d-flex align-items-stretch">
+                  <div class="card">
+                     <div class="card-header bg-white card-header-custom">Member</div>
+                     <img src="assets/images/qc.png" alt="" class="card-img-top">
+                     <div class="card-body bg-white" >
+                        <h5 class="card-title">Nguyễn Mậu Minh Đức</h5>
+                        <p class="card-text">Xin chào mọi người, mình là Minh Đức đây!
+                        </p>
+                     </div>
+                     <div class="card-footer bg-white">
+                        <a href="#" class="btn btn-primary">More Infor</a>
+                     </div>
+                  </div>
+               </div>
+   
+               <div class="card-block col-md-8 col-lg-3 d-flex align-items-stretch">
+                  <div class="card">
+                     <div class="card-header bg-white card-header-custom">Member</div>
+                     <img src="assets/images/qc.png" alt="" class="card-img-top">
+                     <div class="card-body bg-white" >
+                        <h5 class="card-title">Nguyễn Mậu Minh Đức</h5>
+                        <p class="card-text">Xin chào mọi người, mình là Minh Đức đây!
+                        </p>
+                     </div>
+                     <div class="card-footer bg-white">
+                        <a href="#" class="btn btn-primary">More Infor</a>
+                     </div>
+                  </div>
+               </div>
+   
+               <div class="card-block col-md-8 col-lg-3 d-flex align-items-stretch">
+                  <div class="card">
+                     <div class="card-header bg-white card-header-custom">Member</div>
+                     <img src="assets/images/qc.png" alt="" class="card-img-top">
+                     <div class="card-body bg-white" >
+                        <h5 class="card-title">Nguyễn Mậu Minh Đức</h5>
+                        <p class="card-text">Xin chào mọi người, mình là Minh Đức đây!
+                        </p>
+                     </div>
+                     <div class="card-footer bg-white">
+                        <a href="#" class="btn btn-primary">More Infor</a>
+                     </div>
+                  </div>
+               </div> 
+            </div>
+         </div>
+         <div class="box"></div>
+      </div>
+   </section>
+
+<?php
+include_once('views/main/footer.php');
+?>
