3216bd43d5b0f35d6d32ffd037c77a077e6a7050
Hien Giang <quachtrieuvu@gmail.com>
2022-11-19 13:51:54 +0700

    add navbar and footer

---
+++ views/main/navbar.php
@@ -0,0 +1,77 @@
+<!-- Footer -->
+<footer class="text-center text-lg-start bg-light text-muted">
+
+  <!-- Section: Links  -->
+    <div class="container text-center text-md-start mt-5">
+      <!-- Grid row -->
+      <div class="row mt-3">
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4 mt-5">
+          <!-- Content -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            <i class="fas fa-gem"></i>BIG FARM
+          </h6>
+          <p>
+            <span class="fw-bold fst-italic">FASHION FOR ALL</span>
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4 mt-5">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            Products
+          </h6>
+          <p>
+            <a href="#!" class="text-reset">Áo thun</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Áo sơ mi</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Áo khoác</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Váy </a>
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4 mt-5">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            News
+          </h6>
+          <p>
+            <a href="#!" class="text-reset">Sale</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Private Product</a>
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4 mt-5">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
+          <p><i class="fas fa-home"></i>Address: KTX Khu A</p>
+          <p>
+            <i class="fas fa-envelope"></i>Email: bigfarm@gmail.com</p>
+          <p><i class="fas fa-phone"></i>Hotline: 01 234 567 88</p>
+        </div>
+        <!-- Grid column -->
+      </div>
+      <!-- Grid row -->
+    </div>
+  <!-- Section: Links  -->
+
+  <!-- Copyright -->
+  <div class="text-center p-4 fw-bold" style="background-color: rgba(0, 0, 0, 0.05);">
+    <a href="index.php?page=main&controller=layouts&action=index"><span class="red">BIG</span><span class="green">FARM</span> 
+  </div>
+  <!-- Copyright -->
+</footer>
+<!-- Footer -->
