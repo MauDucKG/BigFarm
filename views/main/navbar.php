6ef9297177b2585b283c87b575ee705f3213538c
Hien Giang <quachtrieuvu@gmail.com>
2022-12-14 15:04:47 +0700

    link stylesheet file for service page

--- views/main/navbar.php
+++ views/main/navbar.php
@@ -27,7 +27,8 @@
   <link rel="stylesheet" href="assets/stylesheets/star_rating.css">
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" charset="utf-8"></script>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
-
+  <!-- Link CSS -->
+  <link rel="stylesheet" href="assets/stylesheets/main/services.css">
 </head>
 
 <body>
