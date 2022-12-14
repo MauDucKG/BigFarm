fae00d646750ea89240bbee4e1efcbd415a0e7f8
Hien Giang <quachtrieuvu@gmail.com>
2022-12-14 11:14:14 +0700

    render location from Google map and set phone number

---
+++ assets/javascripts/company/location.js
@@ -0,0 +1,10 @@
+
+let elem = document.getElementsByClassName("location");
+elem[0].href = "https://goo.gl/maps/84fG1ScF8VEUMG8h9";
+elem[0].target = '_blank';
+elem[1].href = 'https://goo.gl/maps/yWH1LJZJFPSakgjH7';
+elem[1].target = '_blank';
+
+let phone = document.getElementsByClassName('phone');
+phone[0].innerHTML = "0 123 678 321";
+phone[1].innerHTML = "0 312 345 434";
