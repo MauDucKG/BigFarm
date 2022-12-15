165e0a3a8ac78e39d27ab4a2513d4ee7fca50b53
Hien Giang <quachtrieuvu@gmail.com>
2022-12-15 16:57:31 +0700

    update responsive design for contact page

--- views/main/contact/index.php
+++ views/main/contact/index.php
@@ -11,18 +11,18 @@ include_once('views/main/navbar.php');
     <!-- <div>
       <iframe style="border:0; width: 100%; height: 350px;" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7839.393507316623!2d106.743711!3d10.757838!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb98f29e0479d526a!2sVNG%20Corporation!5e0!3m2!1sen!2sus!4v1637860473994!5m2!1sen!2sus" frameborder="0" allowfullscreen></iframe>
     </div> -->
-    <div class="row mx-5 mt-2 con">
+    <div class="row mt-2 con">
         <?php
         // href="https://goo.gl/maps/dyhKkLf4aSUYmhQo7" target="_blank"
             foreach ($companies as $company) {
                 echo '
-                <div class="col-3">
+                <div class="col-3 container">
                     <div class="fs-5 fw-bold">CHI NHÁNH</div>
                     <div>'. $company->name.'</div>
                 </div>
                 ';
                 echo '
-                <div class="col-5">
+                <div class="col-5 container">
                     <div class="fs-5 fw-bold">ĐỊA CHỈ</div>
                     <div>
                         <a class="location">'
@@ -32,7 +32,7 @@ include_once('views/main/navbar.php');
                 </div>
                 ';
                 echo '
-                <div class="col-4">
+                <div class="col-4 container">
                     <div class="fs-5 fw-bold">SỐ ĐIỆN THOẠI</div>
                     <div class="phone"></div>
                 </div>
